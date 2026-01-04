d723617bed41495ba2deac0e64ae069313af7c08
shubhamkapse11 <shubhamkapse1110@gmail.com>
2026-01-04 22:51:31 +0530

    feat: add Sequelize User and Task models, refactor CLI commands to `questions.txt`, and update `.gitignore` entries.

--- models/task.js
+++ models/task.js
@@ -11,6 +11,8 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Task.belongsTo(models.User, { foreignKey: 'userId' });
+
     }
   }
   Task.init({
